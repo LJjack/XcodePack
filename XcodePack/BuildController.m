cab180591e26721df36623116c818bac5acb44f0
刘俊杰 <liujunjie@BUJiongdeMacBook-Air.local>
2017-03-24 17:27:34 +0800

    添加上传到蒲公英

--- XcodePack/BuildController.m
+++ XcodePack/BuildController.m
@@ -11,6 +11,7 @@
 
 #import "XcodePack-Swift.h"
 
+#import "AFNetworking.h"
 
 @interface BuildController ()
 
@@ -18,9 +19,7 @@
 @property (weak) IBOutlet BDDragView *dragView;
 @property (weak) IBOutlet NSButton *runBtn;
 @property (weak) IBOutlet NSProgressIndicator *indicator;
-
 @property (weak) IBOutlet NSButton *uploadPgyBtn;
-
 
 @property (nonatomic, copy) NSString *projectPath;
 
@@ -64,6 +63,8 @@
     sender.enabled = NO;
     self.indicator.hidden = NO;
     [self.indicator startAnimation:nil];
+    self.uploadPgyBtn.enabled = NO;
+    
     NSString *project = self.projectPath.lastPathComponent;
     NSString *path = self.projectPath.stringByDeletingLastPathComponent;
     
@@ -78,13 +79,42 @@
             });
             
         };
-        pack.didFinish = ^(int32_t stauts) {
+        pack.didFinish = ^(int32_t stauts, NSString *path) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.indicator stopAnimation:nil];
+                self.uploadPgyBtn.enabled = YES;
+                if (stauts == 0 && self.uploadPgyBtn.state) {
+                    [self uploadPgy:path];
+                }
             });
         };
         [pack build];
     });
+}
+
+- (void)uploadPgy:(NSString *)path {
+    NSString *fileName = [path componentsSeparatedByString:@"/"].lastObject;
+    NSString *urlString = @"https://qiniu-storage.pgyer.com/apiv1/app/upload";
+    NSDictionary *parameters = @{@"uKey":@"c1d70843df29d3f5addd57af95a381ca",
+                                 @"_api_key":@"de68eda891fae6b185d92362f74f6159",
+                                 @"installType":@"2",
+                                 @"password":@"bj123456"};
+    
+    AFHTTPRequestSerializer *serializer = [AFHTTPRequestSerializer serializer];
+    NSMutableURLRequest *request = [serializer multipartFormRequestWithMethod:@"POST" URLString:urlString parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        [formData appendPartWithFileURL:[NSURL fileURLWithPath:path] name:@"file" fileName:fileName mimeType:@"form-data" error:nil];
+    } error:nil];
+    
+    
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] init];
+    
+    NSURLSessionUploadTask * uploadTask = [manager uploadTaskWithStreamedRequest:request progress:^(NSProgress * _Nonnull uploadProgress) {
+        NSLog(@"%f",uploadProgress.fractionCompleted);
+    } completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        NSLog(@"%@  %@", responseObject, error);
+    }];
+    [uploadTask resume];
 }
 
 @end
--- XcodePack/Utils.swift
+++ XcodePack/Utils.swift
@@ -11,7 +11,7 @@ import Cocoa
 fileprivate let kExportOptionsPlist =  Bundle.main.path(forResource: "exportOptions", ofType: "plist")
 //configuration for iOS build setting
 fileprivate let kConfiguration = "Release"
-fileprivate let kExportMainDirectory = "~/Desktop/" //会在桌面创建输出ipa文件的目录
+fileprivate let kOutputPath = "Desktop" //会在桌面创建输出ipa文件的目录
 
 class AutoPack : NSObject {
     
@@ -26,7 +26,7 @@ class AutoPack : NSObject {
     private let project:String
     
     public var runningLog: ((String) -> Void)?
-    public var didFinish: ((Int32) -> Void)?
+    public var didFinish: ((Int32, String) -> Void)?
     
     init(_ filePath:String, project:String) {
         self.filePath = filePath
@@ -42,9 +42,6 @@ class AutoPack : NSObject {
         let archivePath = buildArchivePath(filePath: filePath, name: projectName)
         var cmdCode = "cd " + filePath + "; "
         cmdCode += "xcodebuild -\(typeName) \(project) -scheme \(projectName) -configuration \(kConfiguration) archive -archivePath \(archivePath) -destination generic/platform=iOS; "
-        
-        
-        
         debugPrint("命令代码：" + cmdCode)
         let status = self.cmd.tCmd(cmd:cmdCode) { log in
             if let r = self.runningLog {
@@ -55,10 +52,10 @@ class AutoPack : NSObject {
         }
         if status != 0 {
             if let finish = self.didFinish {
-                finish( status)
+                finish(status, "")
             }
         } else {
-            let exportDirectory = self.buildExportDirectory(projectName)
+            let exportDirectory = self.ipaOutputPath(filePath, projectName: projectName)
             let cmdCodeIpa = "xcodebuild -exportArchive -archivePath \(archivePath) -exportPath \(exportDirectory) -exportOptionsPlist \(kExportOptionsPlist!); "
             debugPrint("命令代码：" + cmdCodeIpa)
             let status1 = self.cmd.tCmd(cmd:cmdCodeIpa) { log in
@@ -67,12 +64,12 @@ class AutoPack : NSObject {
                 }
             }
             if let finish = self.didFinish {
-                finish( status1)
+                let ipaPath = exportDirectory + "/" + projectName + ".ipa"
+                finish(status1,ipaPath)
             }
         }
         cmd.tCmd(cmd: "rm -r \(archivePath);")
     }
-    
 }
 
 extension AutoPack {
@@ -93,13 +90,15 @@ extension AutoPack {
         return typeName
     }
     
-    //创建输出IPA文件路径: ~/Desktop/{scheme}{2016-12-28_08-08-10}
-    fileprivate func buildExportDirectory(_ scheme:String) -> String {
+    //创建输出IPA文件路径: ~/Desktop/{projectName}{2016-12-28_08-08-10}
+    fileprivate func ipaOutputPath(_ filePath:String, projectName:String) -> String {
         let date = Date()
         let format = DateFormatter()
-        format.dateFormat = "yyyy-MM-dd_HH:mm:ss"
+        format.dateFormat = "yyyy-MM-dd_HH-mm-ss"
         let dataString = format.string(from: date)
-        return kExportMainDirectory + scheme + dataString
+        return filePath.components(separatedBy: "/")[1...2].reduce("/") { (res, node) -> String in
+            return res +  node + "/"
+        } + kOutputPath + "/" + projectName + dataString
     }
     
     fileprivate func buildArchivePath(filePath:String,name:String) -> String {
